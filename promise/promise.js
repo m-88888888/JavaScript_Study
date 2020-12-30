f055a542d32b4275c326f8af5a2cefd642ed7f68
m-88888888 <mochi.h8888@gmail.com>
2020-12-30 21:07:12 +0900

    呼び出しタイミングが制御できない

--- promise/promise.js
+++ promise/promise.js
@@ -21,4 +21,19 @@ const callFunctionHell = () => {
   )
 }
 
-callFunctionHell()
+// callFunctionHell()
+
+// 関数の呼び出し順序が制御できない
+const callTiming = () => {
+  asynFunction(100, (value) => {
+    console.log(value)
+  })
+  asynFunction(200, (value) => {
+    console.log(value)
+  })
+  asynFunction(400, (value) => {
+    console.log(value)
+  })
+}
+
+callTiming()
