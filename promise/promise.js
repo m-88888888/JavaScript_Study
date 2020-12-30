9dfdc6452581054266064612a702ca5b4cd633cf
m-88888888 <mochi.h8888@gmail.com>
2020-12-30 21:26:51 +0900

    Promiseで非同期処理の順序を制御

--- promise/promise.js
+++ promise/promise.js
@@ -1,19 +1,19 @@
-const asynFunction = (data, callback) => {
+const asyncFunction = (data, callback) => {
   setTimeout(() => callback(data * 2), Math.random() * 1000)
 }
 
 const callFunction = () => {
-  asynFunction(100, (value) => console.log(value))
+  asyncFunction(100, (value) => console.log(value))
 }
 
 // callFunction()
 
 const callFunctionHell = () => {
-  asynFunction(100, (value) => { 
+  asyncFunction(100, (value) => { 
       console.log(value)
-      asynFunction(value, (value) => {
+      asyncFunction(value, (value) => {
         console.log(value)
-        asynFunction(value, (value) => {
+        asyncFunction(value, (value) => {
           console.log(value)
         })
       })
@@ -25,15 +25,38 @@ const callFunctionHell = () => {
 
 // 関数の呼び出し順序が制御できない
 const callTiming = () => {
-  asynFunction(100, (value) => {
+  asyncFunction(100, (value) => {
     console.log(value)
   })
-  asynFunction(200, (value) => {
+  asyncFunction(200, (value) => {
     console.log(value)
   })
-  asynFunction(400, (value) => {
+  asyncFunction(400, (value) => {
     console.log(value)
   })
 }
 
-callTiming()
+// callTiming()
+
+const asyncPromiseFunction = (data) => {
+  return new Promise((callback) => {
+    setTimeout(() => callback(data * 2), Math.random() * 1000)
+  })
+}
+
+const promiseFunction = () => {
+  asyncPromiseFunction(100)
+    .then((value) => {
+      console.log(value) 
+      return asyncPromiseFunction(value)
+    })
+    .then((value) => {
+      console.log(value)
+      return asyncPromiseFunction(value)
+    })
+    .then((value) => {
+      console.log(value)
+    })
+}
+
+promiseFunction()
